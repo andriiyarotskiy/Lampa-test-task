6db9fa913c50ab4ad7ce3c5fcf7d8f3e9f49a763
Andriy <refwol131@mail.ru>
2020-10-05 07:57:12 +0300

    Updates

---
+++ precache-manifest.baa8975ca9f0056c05a11fd159b0b59a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff20b8a4217203dac9255dc8d6bdf0d0",
+    "url": "/Lampa-test-task/index.html"
+  },
+  {
+    "revision": "1a5c3fb260e058788535",
+    "url": "/Lampa-test-task/static/css/main.845dace8.chunk.css"
+  },
+  {
+    "revision": "0c459cc62ecb159860d5",
+    "url": "/Lampa-test-task/static/js/2.f8e0511f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Lampa-test-task/static/js/2.f8e0511f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5c3fb260e058788535",
+    "url": "/Lampa-test-task/static/js/main.a7237b81.chunk.js"
+  },
+  {
+    "revision": "fc852c19c315c60c10ec",
+    "url": "/Lampa-test-task/static/js/runtime-main.0d3ff7ed.js"
+  }
+]);
